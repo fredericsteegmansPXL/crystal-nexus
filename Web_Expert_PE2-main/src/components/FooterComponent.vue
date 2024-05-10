1185c1d13c5bfdf53e8aeb5e8554fde0d100e88f
fredericsteegmansPXL <frederic.steegmans@student.pxl.be>
2024-05-10 14:42:25 +0200

    scss fix kinda

---
+++ Web_Expert_PE2-main/src/components/FooterComponent.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      lvLogoExtendedFooter: '/src/assets/images/logo_deel_2.png',
+      lvLogoExtendedAltFooter: "Logo afbeelding extended",
+      productNaamFooter: "Product",
+      productArtikelFooter: "All kinds of crystals",
+      contactNaamFooter: "Contact",
+      contactTekstFooter: "Heb je een vraag voor ons?",
+      contactButtonTekstFooter: "Neem contact met ons op!",
+      copyRightFooter: "2023 Lv Visionary Visuals",
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="footer">
+    <div class="footer__section footer__section__links">
+      <img :src="lvLogoExtendedFooter" :alt="lvLogoExtendedAltFooter">
+    </div>
+    <div class="footer__section footer__section__products">
+      <h2 class="footer__titel">{{ productNaamFooter }}</h2>
+      <router-link :to="{name:'producten'}"><p class="footer__tekst">{{  productArtikelFooter }}</p></router-link>
+    </div>
+    <div class="footer__section footer__section__contact">
+      <h2 class="footer__titel">{{ contactNaamFooter }}</h2>
+      <p class="footer__tekst">{{ contactTekstFooter }}</p>
+      <button>{{ contactButtonTekstFooter }}</button>
+    </div>
+  </div>
+  <div class="footer__copy-right">&copy {{ copyRightFooter }}</div>
+</template>
---
+++ Web_Expert_PE2-main/src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      logoSrcNav: '/src/assets/images/logo.png',
+      logoAltNav: "Logo Visionary_Visuals",
+      productNaamNav: "Product",
+      homeNaamNav: "Home",
+    };
+  },
+};
+</script>
+
+<template>
+  <nav class="main__nav">
+    <div class="brand">
+      <router-link to="/"><img :src="logoSrcNav" :alt="logoAltNav"></router-link>
+      <i class="fas fa-bars toggle-icon"></i>
+    </div>
+    <router-link to="/products" class="nav__item router-link-active">{{ productNaamNav }}</router-link>
+    <router-link to="/" class="nav__item router-link-active">{{ homeNaamNav }}</router-link>
+    <router-link to="winkelmandje" class="nav__item"><i class="fa-solid fa-cart-shopping"></i></router-link>
+    <router-link to="login" class="nav__item"><i class="fa-solid fa-user"></i></router-link>
+  </nav>
+</template>
